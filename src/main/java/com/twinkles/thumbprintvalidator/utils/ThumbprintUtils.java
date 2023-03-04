f3953917b04225a5b67b2a162cd29f65e6459eac
Adewale Adeyinka <adeyinkawale13@gmail.com>
2023-03-04 02:18:39 +0100

    Added utils and Security Config class

---
+++ src/main/java/com/twinkles/thumbprintvalidator/utils/ThumbprintUtils.java
@@ -0,0 +1,9 @@
+package com.twinkles.thumbprintvalidator.utils;
+
+import java.security.PublicKey;
+
+public class ThumbprintUtils {
+    public static String calculateThumbprint(PublicKey publicKey) {
+        return null;
+    }
+}
