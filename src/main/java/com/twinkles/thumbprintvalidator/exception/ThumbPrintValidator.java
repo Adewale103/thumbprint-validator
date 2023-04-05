d11b11be891b228ed4d1d87963a9b2fc97f81e84
Adewale Adeyinka <adeyinkawale13@gmail.com>
2023-04-05 18:07:39 +0100

    Added exception

---
+++ src/main/java/com/twinkles/thumbprintvalidator/exception/ThumbPrintValidator.java
@@ -0,0 +1,7 @@
+package com.twinkles.thumbprintvalidator.exception;
+
+public class ThumbPrintValidator extends Exception {
+    public ThumbPrintValidator(String message){
+        super(message);
+    }
+}
